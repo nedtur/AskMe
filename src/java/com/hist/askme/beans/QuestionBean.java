24ec8a7f7b2174c2dc6c565c6454da73e77f3420
Ina Therese Nøklegård <inan@stud.hist.no>
2012-03-13 11:21:35 +0100

    ajax

--- src/java/com/hist/askme/beans/QuestionBean.java
+++ src/java/com/hist/askme/beans/QuestionBean.java
@@ -6,10 +6,12 @@ package com.hist.askme.beans;
 
 import com.hist.askme.models.Question;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.bean.ManagedBean;
-
-
 
 /**
  *
@@ -17,36 +19,47 @@ import javax.faces.bean.ManagedBean;
  */
 @ManagedBean
 @SessionScoped
-
 public class QuestionBean {
-    String [] svar = {"Ja","Nei"};
+
+    String[] svar = {"Ja", "Nei"};
     ArrayList<Question> questions = new ArrayList<Question>();
     Question question = new Question("Funker dette?", 2, svar);
     String out = question.getQuestion();
     String name;
     String password = "ina";
-    
+    private Set<String> types = new TreeSet<String>();
+
     public Question getQuestion() {
         return question;
     }
-    
+
     public String getOut() {
         return out;
     }
-    
+
     public String getName() {
         return name;
     }
-    
+
     public String getPassword() {
         return password;
     }
-    
+
     public void setName(String newName) {
         this.name = newName;
     }
-    
+
     public void setPassword(String newPassword) {
         this.password = newPassword;
     }
+
+    public Set<String> getTypes() {
+        return types;
+    }
+
+    
+    public void setTypes(Set<String> newValue) {
+        types = newValue;
+    }
+    
 }
