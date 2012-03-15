648d432cb4a362da529bafc8f464edcd1126c1f2
Håvard <Hvard@158.38.186.159>
2012-03-15 17:43:39 +0100

    PINKNESS

--- src/java/com/hist/askme/beans/QuestionBean.java
+++ src/java/com/hist/askme/beans/QuestionBean.java
@@ -7,10 +7,6 @@ package com.hist.askme.beans;
 import com.hist.askme.models.Question;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.bean.ManagedBean;
 
@@ -18,101 +14,49 @@ import javax.faces.bean.ManagedBean;
  *
  * @author Håvard
  */
-
 @ManagedBean
 @SessionScoped
 public class QuestionBean {
 
-    String[] svar = {"Ja", "Nei"};
-    ArrayList<Question> questions = new ArrayList<Question>();
-    Question question = new Question("Funker dette?", 2, svar);
-    String out = question.getQuestion();
-    String name;
-    String password = "ina";
-    private Set<String> types = new TreeSet<String>();
+    static final ArrayList<Question> questions = new ArrayList<Question>();
+    String question = "Spørsmål...";
+    int amount = 0;
+    static final ArrayList<String> answers = new ArrayList<String>();
+    String ans = "Svar...";
     
-    public String testSTRING = "spørsmålsTEST";
-    int testINT = 6;
+    public String getQuestion() { return question; }
+    public void setQuestion(String newQ) { question = newQ; }
+    public int getAmount() { return amount; }
+    public void setAmount(int newA) { amount = newA; }
+    public String getAns() { return ans; }
+    public void setAns(String newA) { ans = newA; }
+    public ArrayList<String> getAnswers() { return answers; }
     
-    public String getTestString(){
-        return testSTRING;
+    public String addAnswer() {
+        answers.add(ans);
+        amount++;
+        return null;
     }
     
-    public int getTestInt(){
-        return testINT;
+    public String deleteAnswer(String answer) {
+        answers.remove(answer);
+        amount--;
+        return null;
     }
     
-    public void setTestInt(int ny){
-        testINT = ny;
-    }
-    
-     public void setTestString(String ny){
-            testSTRING = ny;
+    public String addQuestion() {
+        Question q = new Question(this.question, this.amount, answers);
+        questions.add(q);
+        return null;
     }
 
-    public Question getQuestion() {
-        return question;
+    public String deleteQuestion(Question question) {
+        questions.remove(question);
+        return null;
     }
 
-    public String getOut() {
-        return out;
-    }
+    public ArrayList<Question> getQuestions() {
+        return questions;
 
-    public String getName() {
-        return name;
     }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setName(String newName) {
-        this.name = newName;
-    }
-
-    public void setPassword(String newPassword) {
-        this.password = newPassword;
-    }
-
-    public Set<String> getTypes() {
-        return types;
-    }
-
-    public void setTypes(Set<String> newValue) {
-        types = newValue;
-    }
-    
-       public String submit() {
-        return "questionnaire";
-    }
- 
-    final static String[] stringArray = {"tester her", "test", "hei"};
-    
-    public static final ArrayList<Question> questionlist =
-            new ArrayList<Question>(Arrays.asList(
-            new Question("Spørsmål her", 3, stringArray)));
-    
-    public String addAction() {
- 
-		Question questionme = new Question(this.testSTRING, this.testINT, 
-			this.svar);
- 
-		questionlist.add(questionme);
-		return null;
-	}
-    
-    public String deleteAction(Question question) {
- 
-		questionlist.remove(question);
-		return null;
-	}
-    
-    public ArrayList<Question> getQuestionlist() {
- 
-		return questionlist;
- 
-	}
-    
-    
 }
- 
