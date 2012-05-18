edd88c3700fe9c08ca01bdc118b307f5bb062d1a
Håvard <Hvard@158.38.186.159>
2012-05-18 11:54:31 +0200

    ..

--- src/java/com/hist/askme/models/TextQuestion.java
+++ src/java/com/hist/askme/models/TextQuestion.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.*;
 
-
+@Entity
 public class TextQuestion extends Question {
     private List<String> textAnswers = new ArrayList<String>();
      
